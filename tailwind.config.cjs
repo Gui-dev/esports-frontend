a57324ab3e9b44e6fec470fd1a1a492e790e6493
Gui-dev <adrianon10@gmail.com>
2022-09-24 04:27:19 -0300

    feat: add game-gradient and font Inter

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,8 +8,12 @@ module.exports = {
     extend: {
       backgroundImage: {
         backgroundGalaxy: "url('/background.png')",
-        gradient: 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD, 60.94%, #E1D55D 20.57%)'
+        'gradient-text': 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD, 60.94%, #E1D55D 20.57%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, .9) 67.08%)'
       }
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
     }
   },
   plugins: []
