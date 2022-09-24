7596ccf74b87f603542948b267c3fd4372f4e5f3
Gui-dev <adrianon10@gmail.com>
2022-09-24 03:28:25 -0300

    feat: add background and gradient text

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    '.src/**/*.tsx'
+    './src/**/*.tsx',
+    './index.html'
   ],
   theme: {
-    extend: {}
+    extend: {
+      backgroundImage: {
+        backgroundGalaxy: "url('/background.png')",
+        gradient: 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD, 60.94%, #E1D55D 20.57%)'
+      }
+    }
   },
   plugins: []
 }
