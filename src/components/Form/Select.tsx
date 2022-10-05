47c0b3382685c34c373fcfab4cbcf04b2b46948b
Gui-dev <adrianon10@gmail.com>
2022-10-05 03:29:25 -0300

    feat: add Select component

---
+++ src/components/Form/Select.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import * as Select from '@radix-ui/react-select'
+import { CaretDown } from 'phosphor-react'
+
+type GamesProps = {
+  id: string
+  title: string
+  bannerUrl: string
+}
+
+type InputSelectProps = {
+  games: GamesProps[]
+}
+
+export const InputSelect = ({ games }: InputSelectProps) => {
+  return (
+    <div className="flex py-3 px-4 bg-zinc-900 rounded">
+      <Select.Root>
+        <Select.Trigger className="flex flex-1 items-center justify-between">
+          <Select.Value className="placeholder:text-zinc-500" placeholder="Selecione o game que deseja jogar" />
+          <Select.Icon>
+            <CaretDown className="text-white" />
+          </Select.Icon>
+        </Select.Trigger>
+
+        <Select.Portal className=" w-full bg-zinc-700">
+          <Select.Content className="">
+            <Select.Viewport>
+              <Select.Group>
+
+                {games.map(game => {
+                  return (
+                    <Select.Item
+                      key={game.id}
+                      value={game.title}
+                      className="text-white py-2 px-4 cursor-pointer hover:bg-violet-500"
+                    >
+                      <div className="flex flex-row items-center justify-between">
+                        <Select.ItemText className="text-white">{game.title}</Select.ItemText>
+                        <img src={game.bannerUrl} className="h-6 w-6" />
+                      </div>
+                      <Select.Separator className="self-stretch my-1 h-[1px] w-full bg-zinc-600" />
+                    </Select.Item>
+                  )
+                })}
+
+              </Select.Group>
+            </Select.Viewport>
+          </Select.Content>
+        </Select.Portal>
+      </Select.Root>
+    </div >
+  )
+}
